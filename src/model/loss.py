1095d43680f4886b8cab53b98b5722cf8983ad33
Jinen Setpal <jinens8@gmail.com>
2023-09-22 23:01:29 -0400

    corrected loss function

--- src/model/loss.py
+++ src/model/loss.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from astropy.convolution import Gaussian2DKernel
-import torch.nn.functional as F
 import torch.nn as nn
 from .. import const
 import numpy as np
@@ -13,9 +12,10 @@ class CAMLoss(nn.Module):
         super().__init__()
         self.kernel = torch.tensor(np.array(Gaussian2DKernel(const.IMAGE_SIZE[0] * .01, x_size=shape[0], y_size=shape[1]))).to(const.DEVICE)
         self.kernel -= self.kernel.max()
+        self.kernel = torch.square(self.kernel)
 
     def forward(self, y_pred, _):
-        return torch.log(torch.square(F.relu(y_pred[:, 0]) * self.kernel).mean())
+        return torch.square(y_pred[:, 0] * self.kernel).mean()
 
 
 if __name__ == '__main__':
