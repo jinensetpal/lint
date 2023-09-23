fixed normalization bug

src/dataset.py
@@ -25,7 +25,7 @@ class Dataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         X = torchvision.transforms.functional.resize(torchvision.io.read_image((const.DATA_DIR / self.df['img_filename'].iloc[idx]).as_posix()), const.IMAGE_SIZE, antialias=True)
-        X = X / X.max()  # normalization
+        X = X / 255  # normalization
         y = torch.tensor([self.df['y'][idx]]).to(torch.float)
 
         if self.state == 'evaluation': return X, y, torch.tensor(self.df['place'][idx])