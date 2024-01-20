d3491abbad27de32c2ddf541d269b35537331c56
Jinen Setpal <jinens8@gmail.com>
2024-01-20 10:52:05 -0500

    switched to hi-res cams

--- src/classification/arch.py
+++ src/classification/arch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from torchvision.models.resnet import resnet50
 from itertools import pairwise
 from src import const
 from torch import nn
@@ -10,40 +11,32 @@ class Model(torch.nn.Module):
     def __init__(self, input_shape):
         super().__init__()
 
-        self.convs = nn.ModuleList([nn.Conv2d(*filters, 2, padding='same') for filters in pairwise([3, 16, 64, 32])])
-        self.batchnorms = nn.ModuleList([nn.BatchNorm2d(channels) for channels in [16, 64, 32]])
-        self.avgpool = nn.AvgPool2d(2)
-
-        self.penultimate = None
-        self.flatten = nn.Flatten()
-
-        self.linears = nn.ModuleList([nn.LazyLinear(units) for units in [128, 128, 64, 32, 2]])
-        self.relu = nn.ReLU()
+        self.feature_grad = None
+        self.feature_vect = None
+        self.backbone = resnet50()
+        self.backbone.fc = nn.LazyLinear(2)
         self.softmax = nn.Softmax(dim=1)  # ~equivalent to sigmoid since classes = 2; for CAMs
+        self.backbone.layer4[-1].conv3.register_forward_hook(self.hook)
+
+    def hook(self, model, i, o):
+        def assign(grad):
+            self.feature_grad = grad
+        self.feature_vect = o
+        o.register_hook(assign)
 
     def forward(self, x):
-        for conv, bn in zip(self.convs, self.batchnorms):
-            x = conv(x)
-            self.penultimate = x
-            x = bn(x)
-            x = self.avgpool(x)
-        x = self.flatten(x)
-
-        for linear in self.linears[:-1]:
-            x = linear(x)
-            x = self.relu(x)
-
-        x = self.linears[-1](x)
+        x = self.backbone(x)
         x = self.softmax(x)
 
-        return x, self._compute_cam(torch.argmax(x, dim=1))
+        return x, self._compute_hrs_cam(x)
 
-    def _compute_cam(self, label_index):
-        class_weights = self.linears[-1].weight
-        def single_cam(penultimate):
-            return torch.vmap(lambda x: torch.tensordot(x, penultimate, dims=1), in_dims=0)(class_weights)
-
-        return torch.vmap(single_cam, in_dims=0)(self.penultimate)
+    def _compute_hrs_cam(self, y):
+        cams = torch.zeros(*y.shape, 7, 7)
+        for img_idx in range(y.shape[0]):
+            for class_idx in range(y.shape[1]):
+                (y[img_idx, class_idx]).backward(retain_graph=True)
+                cams[img_idx, class_idx] = (self.feature_vect * self.feature_grad).sum(1)[img_idx]
+        return cams
 
 
 if __name__ == '__main__':
@@ -52,5 +45,5 @@ if __name__ == '__main__':
     print(model)
 
     x = torch.rand((3, *const.IMAGE_SHAPE)).to(const.DEVICE)
-    y, cam = model(x)
-    print(cam.shape)
+    y, hrs_cam = model(x)
+    print(hrs_cam.shape)
